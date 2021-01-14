5585a1ce72fe3789c95ef1d7eaa1934c2071417d
ViktorVx <petrov.v.anatolyev@sberbank.ru>
2021-01-14 09:05:58 +0300

    Init

---
+++ src/main/java/org/pva/hibernateLearning/HibernateLearningApplication.java
@@ -0,0 +1,13 @@
+package org.pva.hibernateLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pva/hibernateLearning/HibernateLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pva.hibernateLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
