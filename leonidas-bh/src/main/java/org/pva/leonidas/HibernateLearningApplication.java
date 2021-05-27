36a36e754e47a3e1edf6afc1656d43bde8cca62c
ViktorVx <petrov.v.anatolyev@sberbank.ru>
2021-05-27 13:16:08 +0300

    Refactoring: hibernateLearning -> leonidas

--- leonidas-bh/src/main/java/org/pva/leonidas/HibernateLearningApplication.java
+++ leonidas-bh/src/main/java/org/pva/leonidas/HibernateLearningApplication.java
@@ -1,4 +1,4 @@
-package org.pva.hibernateLearning;
+package org.pva.leonidas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
