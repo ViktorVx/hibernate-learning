a6043f3c295a6c5a8c208e328334bee00f02b5df
ViktorVx <petrov.v.anatolyev@sberbank.ru>
2021-05-28 14:38:14 +0300

    Renaming refactoring

--- leonidas-bh/src/main/java/org/pva/leonidas/LeonidasApplication.java
+++ leonidas-bh/src/main/java/org/pva/leonidas/LeonidasApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HibernateLearningApplication {
+public class LeonidasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HibernateLearningApplication.class, args);
+		SpringApplication.run(LeonidasApplication.class, args);
 	}
 
 }
