f2bd33e1248f9d4de47e75a20288960f0cb8ec40
ViktorVx <petrov.v.anatolyev@sberbank.ru>
2021-05-31 18:30:17 +0300

    Add table for TelegramUser

--- liquibase-scripts/postgres/sql/telegram_user.sql
+++ liquibase-scripts/postgres/sql/telegram_user.sql
@@ -1,9 +1,25 @@
---liquibase formatted sql
+-- liquibase formatted sql
 
---changeset person:001 author:ViktorVx runOnChange:true endDelimiter:/
-create table telegram_user (
-telegram_user_id varchar(255) not null,
-user_name varchar(255),
-user_info varchar(255)
-);
+/*** create table SM_OPERATION_VERSION ***/
+-- changeset viktor-vx:LDS_TELEGRAM_USER#0001 dbms:postgresql logicalFilePath:path-independent runOnChange:true splitStatements:true endDelimiter:/
+DO $$
+    DECLARE
+        tableExists boolean;
+    BEGIN
+        SELECT EXISTS (SELECT FROM information_schema.columns cl
+        WHERE cl.table_name = lower('LDS_TELEGRAM_USER') AND cl.table_schema = lower('${schema}')) INTO tableExists;
+        IF (tableExists = FALSE) THEN
+            CREATE TABLE IF NOT EXISTS ${schema}.LDS_TELEGRAM_USER (
+                TELEGRAM_USER_ID VARCHAR(255) NOT NULL,
+                USER_NAME VARCHAR(255),
+                USER_INFO VARCHAR(255),
+                CONSTRAINT LDS_PK_TELEGRAM_USER_TELEGRAM_USER_ID PRIMARY KEY (TELEGRAM_USER_ID) USING INDEX TABLESPACE LDS_TS_IDX
+            ) TABLESPACE LDS_TS_DATA;
+
+            COMMENT ON TABLE ${schema}.LDS_TELEGRAM_USER                   IS 'Данные о пользователе Telegram';
+            COMMENT ON COLUMN ${schema}.LDS_TELEGRAM_USER.USER_NAME        IS 'Имя пользователя';
+            COMMENT ON COLUMN ${schema}.LDS_TELEGRAM_USER.USER_INFO        IS 'Телефонный номер';
+        END IF;
+    END;
+$$;
 /
