deac76a6cf26931630fd0d46b2b529ea3bd46923
ViktorVx <petrov.v.anatolyev@sberbank.ru>
2021-06-08 18:52:34 +0300

    Add leonidas-telegram-bot template

---
+++ leonidas-telegram-bot/src/test/java/org/pva/leonidas/telegram/bot/LeonidasTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pva.leonidas.telegram.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonidasTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
