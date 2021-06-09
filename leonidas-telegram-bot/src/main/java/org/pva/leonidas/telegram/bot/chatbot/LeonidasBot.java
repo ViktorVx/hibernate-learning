9dca3db6d1d7eae3ec8eddf31909a6324ec13688
ViktorVx <petrov.v.anatolyev@sberbank.ru>
2021-06-09 17:01:40 +0300

    Complete simple telegram bot

--- leonidas-telegram-bot/src/main/java/org/pva/leonidas/telegram/bot/chatbot/LeonidasBot.java
+++ leonidas-telegram-bot/src/main/java/org/pva/leonidas/telegram/bot/chatbot/LeonidasBot.java
@@ -1,24 +1,33 @@
 package org.pva.leonidas.telegram.bot.chatbot;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
+@Slf4j
 public class LeonidasBot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.name}")
+    private String botName;
+    @Value("${telegram.bot.token}")
+    private String botToken;
 
     @Override
     public String getBotUsername() {
-        return null;
+        return botName;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return botToken;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        if (update != null && update.getMessage() != null)
+            log.info(update.getMessage().getText());
     }
 }
