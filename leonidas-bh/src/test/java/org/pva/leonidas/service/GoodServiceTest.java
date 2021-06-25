7c55e311224ec9e6b68e8d59a2438e04c6e58644
ViktorVx <petrov.v.anatolyev@sberbank.ru>
2021-06-25 13:32:23 +0300

    Fix tests

--- leonidas-bh/src/test/java/org/pva/leonidas/service/GoodServiceTest.java
+++ leonidas-bh/src/test/java/org/pva/leonidas/service/GoodServiceTest.java
@@ -6,6 +6,7 @@ import org.pva.leonidas.common.dto.GoodDto;
 import org.pva.leonidas.mapper.GoodMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class GoodServiceTest {
 
     @Autowired
